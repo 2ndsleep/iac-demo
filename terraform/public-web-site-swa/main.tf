faba813d479f4777a0864a719462a4db91681c53
Blaine Perry <blaine@secondsleep.io>
2024-03-20 16:29:26 -0400

    Use azurerm_static_web_app resource

--- terraform/public-web-site-swa/main.tf
+++ terraform/public-web-site-swa/main.tf
@@ -3,12 +3,12 @@ resource "azurerm_resource_group" "public_site" {
   location = "East US 2"
 }
 
-resource "azurerm_static_site" "public_site" {
+resource "azurerm_static_web_app" "public_site" {
   name = "scrm-webpub-prd-1"
   resource_group_name = azurerm_resource_group.public_site.name
   location = azurerm_resource_group.public_site.location
 }
 
 output "static_site_hostname" {
-  value = azurerm_static_site.public_site.default_host_name
+  value = azurerm_static_web_app.public_site.default_host_name
 }
